8113052d03a22fb06fc7f129dff0a5ef99743bfb
Luke Peltier <Luke.Peltier@microchip.com>
2024-02-26 15:27:55 -0600

    More

---
+++ lua/luke/set.lua
@@ -0,0 +1,23 @@
+
+vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+vim.opt.nu = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.updatetime = 50
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,7 +3,12 @@ return {
         'nvim-telescope/telescope.nvim', tag = '0.1.5',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
-    "navarasu/onedark.nvim",
+    {
+        "navarasu/onedark.nvim",
+        config = function ()
+            require('onedark').load()
+        end
+    },
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
@@ -43,7 +48,54 @@ return {
     {
         "tpope/vim-fugitive",
         config = function ()
-            vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+            vim.keymap.set('n', '<leader>gg', vim.cmd.Git)
         end
+    },
+    --- Uncomment the two plugins below if you want to manage the language servers from neovim
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        config = function ()
+            local lsp = require("lsp-zero")
+            lsp.on_attach(function(client, bufnr)
+                -- see :help lsp-zero-keybindings
+                -- to learn the available actions
+                lsp.default_keymaps({buffer = bufnr, remap=false})
+            end)
+
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+            ensure_installed = {
+                "lua_ls",
+                "tsserver",
+                "eslint",
+                "bashls",
+                "clangd",
+                "dockerls",
+                "gopls",
+                "html",
+                "jsonls",
+                "jdtls",
+                "marksman",
+                "intelephense",
+                "jedi_language_server",
+                "rust_analyzer",
+                "sqlls",
+                "svelte",
+                "tailwindcss"
+            },
+            handlers = { lsp.default_setup,}
+        })
+        end
+    },
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+    {
+        'ThePrimeagen/vim-be-good',
     }
 }
