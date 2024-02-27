586c6367f625921c5992c8cf97fcbbf984203d95
Luke Peltier <luke@lukepeltier.com>
2024-02-26 19:30:49 -0600

    Fix issues

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -60,35 +60,59 @@ return {
         branch = 'v3.x',
         config = function ()
             local lsp = require("lsp-zero")
+            lsp.extend_lspconfig()
             lsp.on_attach(function(client, bufnr)
                 -- see :help lsp-zero-keybindings
                 -- to learn the available actions
                 lsp.default_keymaps({buffer = bufnr, remap=false})
             end)
 
-            require('mason').setup({})
+           ---  require('mason').setup({})
+            require("mason").setup {
+                log_level = vim.log.levels.DEBUG
+            }
             require('mason-lspconfig').setup({
-            ensure_installed = {
-                "lua_ls",
-                "tsserver",
-                "eslint",
-                "bashls",
-                "clangd",
-                "dockerls",
-                "gopls",
-                "html",
-                "jsonls",
-                "jdtls",
-                "marksman",
-                "intelephense",
-                "jedi_language_server",
-                "rust_analyzer",
-                "sqlls",
-                "svelte",
-                "tailwindcss"
-            },
-            handlers = { lsp.default_setup,}
-        })
+                ensure_installed = {
+                    "lua_ls",
+                    "tsserver",
+                    "eslint",
+                    "bashls",
+                    "clangd",
+                    "dockerls",
+                    "gopls",
+                    "html",
+                    "jsonls",
+                    "jdtls",
+                    "marksman",
+                    "intelephense",
+                    "pyright",
+                    "rust_analyzer",
+                    "sqlls",
+                    "svelte",
+                    "tailwindcss"
+                },
+                handlers = { lsp.default_setup,}
+            })
+            local cmp = require('cmp')
+            local cmp_action = lsp.cmp_action()
+
+            cmp.setup({
+                mapping = cmp.mapping.preset.insert({
+                    -- `Enter` key to confirm completion
+                    ['<Tab>'] = cmp.mapping.confirm({select = false}),
+
+                    -- Ctrl+Space to trigger completion menu
+                    ['<C-Space>'] = cmp.mapping.complete(),
+
+                    -- Navigate between snippet placeholder
+                    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+                    -- Scroll up and down in the completion documentation
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                })
+            })
         end
     },
     {'neovim/nvim-lspconfig'},
