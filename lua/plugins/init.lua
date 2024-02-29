f5516d8bbe3e95370150064580154c4e05bda9d3
Luke Peltier <Luke.Peltier@microchip.com>
2024-02-29 11:10:34 -0600

    Add comment package

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -65,6 +65,29 @@ return {
                 -- see :help lsp-zero-keybindings
                 -- to learn the available actions
                 lsp.default_keymaps({buffer = bufnr, remap=false})
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					pattern = "*.go",
+					callback = function()
+						local params = vim.lsp.util.make_range_params()
+						params.context = {only = {"source.organizeImports"}}
+						-- buf_request_sync defaults to a 1000ms timeout. Depending on your
+						-- machine and codebase, you may want longer. Add an additional
+						-- argument after params if you find that you have to write the file
+						-- twice for changes to be saved.
+						-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+						local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+						for cid, res in pairs(result or {}) do
+							for _, r in pairs(res.result or {}) do
+								if r.edit then
+									local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+									vim.lsp.util.apply_workspace_edit(r.edit, enc)
+								end
+							end
+						end
+						vim.lsp.buf.format({async = false})
+					end
+				})
+                vim.b.lsp_zero_enable_autoformat = 1
             end)
 
            ---  require('mason').setup({})
@@ -119,7 +142,12 @@ return {
     {'hrsh7th/cmp-nvim-lsp'},
     {'hrsh7th/nvim-cmp'},
     {'L3MON4D3/LuaSnip'},
-    {
-        'ThePrimeagen/vim-be-good',
-    }
+	{
+		'numToStr/Comment.nvim',
+		opts = {
+			-- add any options here
+		},
+		lazy = false,
+	}
+
 }
