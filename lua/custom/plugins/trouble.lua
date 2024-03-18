04036218e1938208714eb523651c8f37893bd38e
Luke Peltier <Luke.Peltier@microchip.com>
2024-03-18 09:05:26 -0500

    Plugins from work config

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -6,4 +6,12 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
   },
+  config = function()
+    vim.keymap.set('n', '<leader>xx', function()
+      require('trouble').toggle()
+    end)
+    vim.keymap.set('n', '<leader>xq', function()
+      require('trouble').toggle 'quickfix'
+    end)
+  end,
 }
