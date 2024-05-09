16d88eae89fe9e6ad436fff938cdf93e5ce77a67
Luke Peltier <Luke.Peltier@microchip.com>
2024-05-09 16:15:23 -0500

    Readd nvim-tree

---
+++ lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,13 @@
+return {
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('nvim-tree').setup {}
+    vim.keymap.set('n', '<leader>pv', vim.cmd.NvimTreeToggle)
+  end,
+}
