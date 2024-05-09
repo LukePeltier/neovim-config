cd7afe4ca7a75abdb68a7c1dfebe2cc4a7ad7128
Luke Peltier <Luke.Peltier@microchip.com>
2024-05-09 16:37:26 -0500

    Fix tree to close on file open

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -7,7 +7,13 @@ return {
     'MunifTanjim/nui.nvim',
   },
   config = function()
-    require('nvim-tree').setup {}
+    require('nvim-tree').setup {
+      actions = {
+        open_file = {
+          quit_on_open = true,
+        },
+      },
+    }
     vim.keymap.set('n', '<leader>pv', vim.cmd.NvimTreeToggle)
   end,
 }
