e4747ec8f85fdc386291c4c0377e0ac53a8c3157
Luke Peltier <luke@lukepeltier.com>
2024-05-15 21:36:24 -0500

    try oil

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,19 +1,20 @@
-return {
-  'nvim-tree/nvim-tree.lua',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
-    'MunifTanjim/nui.nvim',
-  },
-  config = function()
-    require('nvim-tree').setup {
-      actions = {
-        open_file = {
-          quit_on_open = true,
-        },
-      },
-    }
-    vim.keymap.set('n', '<leader>pv', vim.cmd.NvimTreeToggle)
-  end,
-}
+-- return {
+--   'nvim-tree/nvim-tree.lua',
+--   version = '*',
+--   dependencies = {
+--     'nvim-lua/plenary.nvim',
+--     'nvim-tree/nvim-web-devicons',
+--     'MunifTanjim/nui.nvim',
+--   },
+--   config = function()
+--     require('nvim-tree').setup {
+--       actions = {
+--         open_file = {
+--           quit_on_open = true,
+--         },
+--       },
+--     }
+--     vim.keymap.set('n', '<leader>pv', vim.cmd.NvimTreeToggle)
+--   end,
+-- }
+return {}
