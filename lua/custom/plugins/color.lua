aff6f9d4154a5ae589a92b2fc20b4c9f7446a1a8
Luke Peltier <Luke.Peltier@microchip.com>
2024-06-06 12:39:56 -0500

    Update colors, add flash and temple

--- lua/custom/plugins/color.lua
+++ lua/custom/plugins/color.lua
@@ -1,43 +1,43 @@
-return { -- You can easily change to a different colorscheme.
-  'folke/tokyonight.nvim',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    vim.cmd.colorscheme 'tokyonight-night'
-    require('tokyonight').setup {
-      style = 'night',
-    }
-  end,
-}
--- return {
---   'catppuccin/nvim',
---   name = 'catppuccin',
---   lazy = false,
---   priority = 1000,
+-- return { -- You can easily change to a different colorscheme.
+--   'folke/tokyonight.nvim',
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
 --   config = function()
---     require('catppuccin').setup {
---       flavor = 'mocha',
---       integrations = {
---         diffview = true,
---         fidget = true,
---         harpoon = true,
---         cmp = true,
---         mason = true,
---         noice = true,
---         gitsigns = true,
---         treesitter = true,
---         telescope = true,
---         lsp_trouble = true,
---         which_key = true,
---         notify = true,
---         mini = {
---           enabled = true,
---           indentscope_color = '',
---         },
---         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
---       },
+--     vim.cmd.colorscheme 'tokyonight-night'
+--     require('tokyonight').setup {
+--       style = 'night',
 --     }
---     vim.o.termguicolors = true
---     vim.cmd.colorscheme 'catppuccin'
 --   end,
 -- }
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('catppuccin').setup {
+      flavor = 'mocha',
+      integrations = {
+        diffview = true,
+        fidget = true,
+        harpoon = true,
+        cmp = true,
+        mason = true,
+        noice = true,
+        gitsigns = true,
+        treesitter = true,
+        telescope = true,
+        lsp_trouble = true,
+        which_key = true,
+        notify = true,
+        mini = {
+          enabled = true,
+          indentscope_color = '',
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+      },
+    }
+    vim.o.termguicolors = true
+    vim.cmd.colorscheme 'catppuccin'
+  end,
+}
