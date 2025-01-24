624db35b251411945acb80823c986bacc19cf850
Luke Peltier <Luke.Peltier@microchip.com>
2025-01-24 11:09:06 -0600

    remove message from lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -27,20 +27,8 @@ return {
       lualine_c = { 'filename' },
       lualine_x = {
         {
-          require('noice').api.status.message.get,
-          cond = require('noice').api.status.message.has,
-        },
-        {
           require('noice').api.status.command.get,
           cond = require('noice').api.status.command.has,
-        },
-        {
-          require('noice').api.status.mode.get,
-          cond = require('noice').api.status.mode.has,
-        },
-        {
-          require('noice').api.status.search.get,
-          cond = require('noice').api.status.search.has,
         },
       },
       lualine_y = { 'encoding', 'fileformat', 'filetype' },
