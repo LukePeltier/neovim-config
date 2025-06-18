27e1a51fd25ef7281c2bb0396400dab8f04755fb
Luke Peltier <luke@lukepeltier.com>
2025-06-18 13:24:00 -0500

    automated dev commit

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -1,7 +1,7 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
   dependencies = { { 'nvim-treesitter/nvim-treesitter', branch = 'main' }, 'nvim-tree/nvim-web-devicons' },
-  ft = { 'markdown', 'codecompanion' },
+  ft = { 'markdown', 'codecompanion', 'Avante' },
   opts = {
     render_modes = true,
     sign = {
