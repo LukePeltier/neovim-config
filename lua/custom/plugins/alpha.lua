d0b66c67d550e7077ac9d6fdb27d4f6c872894f7
Luke Peltier <luke.peltier@microchip.com>
2025-08-25 09:01:34 -0500

    Remove alpha - opening window

--- lua/custom/plugins/alpha.lua
+++ lua/custom/plugins/alpha.lua
@@ -1,22 +1,22 @@
 return {
-  'goolord/alpha-nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-    'nvim-lua/plenary.nvim',
-  },
-  lazy = false,
-  config = function()
-    local alpha = require 'alpha'
-    local dashboard = require 'alpha.themes.dashboard'
-    local theta = require 'alpha.themes.theta'
-    theta.buttons.val = {
-      dashboard.button('e', '  New file', '<cmd>ene<CR>'),
-      dashboard.button('SPC s f', '󰈞  Find file'),
-      dashboard.button('SPC s g', '󰊄  Live grep'),
-      dashboard.button('c', '  Configuration', '<cmd>cd ~/.config/nvim/ <CR>'),
-      dashboard.button('u', '  Update plugins', '<cmd>Lazy sync<CR>'),
-      dashboard.button('q', '󰅚  Quit', '<cmd>qa<CR>'),
-    }
-    alpha.setup(theta.config)
-  end,
+  -- 'goolord/alpha-nvim',
+  -- dependencies = {
+  --   'nvim-tree/nvim-web-devicons',
+  --   'nvim-lua/plenary.nvim',
+  -- },
+  -- lazy = false,
+  -- config = function()
+  --   local alpha = require 'alpha'
+  --   local dashboard = require 'alpha.themes.dashboard'
+  --   local theta = require 'alpha.themes.theta'
+  --   theta.buttons.val = {
+  --     dashboard.button('e', '  New file', '<cmd>ene<CR>'),
+  --     dashboard.button('SPC s f', '󰈞  Find file'),
+  --     dashboard.button('SPC s g', '󰊄  Live grep'),
+  --     dashboard.button('c', '  Configuration', '<cmd>cd ~/.config/nvim/ <CR>'),
+  --     dashboard.button('u', '  Update plugins', '<cmd>Lazy sync<CR>'),
+  --     dashboard.button('q', '󰅚  Quit', '<cmd>qa<CR>'),
+  --   }
+  --   alpha.setup(theta.config)
+  -- end,
 }
